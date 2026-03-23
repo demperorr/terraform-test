d2f5ee929d21576373c8db9e283dd1831616b222
daniel <nonsiel@yahoo.com>
2026-03-23 05:13:31 +0000

    v2 updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "web" {
   ami           = "ami-0453ec754f44f9a4a"
-  subnet_id     = "subnet-0a4e0945b1776917b"
   instance_type = "t3.micro"
 
   tags = {
@@ -24,7 +23,6 @@ resource "aws_instance" "web" {
 
 resource "aws_instance" "web2" {
   ami           = "ami-0453ec754f44f9a4a"
-  subnet_id     = "subnet-0db183c6831fd5738"
   instance_type = "t2.micro"
 
   tags = {
