bba4e7e784bd255aeac5ee8eed1bb598128a4595
daniel <nonsiel@yahoo.com>
2026-03-24 02:53:49 +0000

    ec2 & s3 files  commented out

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "bucks3" {
-  bucket = "my-tf-test-s3bucket0001"
+# resource "aws_s3_bucket" "bucks3" {
+#   bucket = "my-tf-test-s3bucket0001"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
